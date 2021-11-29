d726a06f7e2d46c69c4d8088802226969aa088d4
ashok <hi@apinweb.com>
2021-11-29 00:16:48 -0500

    Login page added

--- FinalProjectiOS/ViewController.swift
+++ FinalProjectiOS/ViewController.swift
@@ -7,13 +7,113 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Field:Codable {
+    let email: String
+    let password: String
+    let name: String
+}
 
+struct Record: Codable {
+    let id: String
+    let fields : Field
+    let createdTime : String
+}
+
+struct ApiResponse : Codable {
+    let records : [Record]
+}
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    let baseUrl = "https://api.airtable.com/v0/appVSEpL2J0yCcuvG"
+    let apiToken =  "Bearer keylEUbJyvrvQ9LLS"
+//    keylEUbJyvrvQ9LLS
+    var isDataLoading = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func handleSignInClick(_ sender: UIButton) {
+   
+        //Reset the values before validation
+        passwordLabel.isHidden = true
+        emailLabel.isHidden = true
 
+        //Check whether the input fields are empty or not
+        var isValid = true
+        if emailAddress.text == "" {
+            isValid = false
+            emailLabel.text = "Email is required"
+            emailLabel.isHidden = false
+        }
+        if password.text == "" {
+            isValid = false
+            passwordLabel.text = "Password is required"
+            passwordLabel.isHidden = false
+        }
+        
+        if !isValid {
+            return
+        }
+        
+        if isDataLoading {
+            return
+        }
+        let semaphore = DispatchSemaphore (value: 0)
+
+        
+        let emailText = String(emailAddress.text!)
+        let passwordText = String(password.text!)
+
+        //Make api call
+        let apiUrl = "https://api.airtable.com/v0/appVSEpL2J0yCcuvG/Users?filterByFormula=AND(({email}='"+emailText+"'),({password}='"+passwordText+"'))"
+ 
+        var request = URLRequest(url: URL(string: apiUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!,timeoutInterval: Double.infinity)
+
+        request.addValue(apiToken, forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("brw=brwD8OHuk7iMnJBzj", forHTTPHeaderField: "Cookie")
+        sender.setTitle("Loading ...", for: .normal)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            semaphore.signal()
+            return
+          }
+            let jsonDecoder = JSONDecoder()
+            do {
+                let apiResponse = try jsonDecoder.decode(ApiResponse.self,from: data)
+                //No users are found
+                if(apiResponse.records.count == 0) {
+                    semaphore.signal()
+                    DispatchQueue.main.async {
+                        self.passwordLabel.text = "Email or password is incorrect"
+                        self.passwordLabel.isHidden = false
+                    }
+                 
+                    return
+                }
+                
+            }catch let jsonError {
+                print(jsonError)
+            }
+          print(String(data: data, encoding: .utf8)!)
+          semaphore.signal()
+        }
+    
+        task.resume()
+        semaphore.wait()
+        sender.setTitle("Sign In", for: .normal)
+
+
+        
+    }
+    
+    
 }
 
